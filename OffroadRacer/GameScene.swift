931bbbd481759f4d11eaa1de1dc6ec059aeb6315
LCS Game Design <tom.shiflet@mylcs.com>
2021-01-13 13:25:16 -0600

    Initial Change State Flow

--- OffroadRacer/GameScene.swift
+++ OffroadRacer/GameScene.swift
@@ -13,17 +13,36 @@ class GameScene: SKScene {
     var gameState = -1
     
     var mmAnchor=SKSpriteNode()
+    var crAnchor=SKSpriteNode()
+    var srAnchor=SKSpriteNode()
+    var erAnchor=SKSpriteNode()
+    var irAnchor=SKSpriteNode()
     
+    var hudAnchor=SKSpriteNode()
+    
+    var hud=HUDClass()
+    
+    var cam=SKCameraNode()
     
 
     
     override func didMove(to view: SKView) {
         
+        addChild(cam)
+        self.camera=cam
+        
+        
+        // add anchors
+        cam.addChild(hudAnchor)
         addChild(mmAnchor)
+        addChild(crAnchor)
+        addChild(erAnchor)
+        addChild(irAnchor)
+        addChild(srAnchor)
+        
 
         changeGameState(to: GAMESTATE.MAINMENU)
-    }
-    
+    }// didMove
     
     func touchDown(atPoint pos : CGPoint) {
         
@@ -34,6 +53,18 @@ class GameScene: SKScene {
         {
         case GAMESTATE.MAINMENU:
             handleClickMM(at: pos)
+            
+        case GAMESTATE.CHOOSERACE:
+            changeGameState(to: GAMESTATE.STARTRACE)
+            
+        case GAMESTATE.STARTRACE:
+            changeGameState(to: GAMESTATE.INRACE)
+            
+        case GAMESTATE.INRACE:
+            changeGameState(to: GAMESTATE.ENDRACE)
+            
+        case GAMESTATE.ENDRACE:
+            changeGameState(to: GAMESTATE.CHOOSERACE)
             
         default:
             print("Error - Unhandled click in gameState \(gameState).")
@@ -64,6 +95,17 @@ class GameScene: SKScene {
     {
         // This function is the handler for mouse clicks on the main menu
         
+        for node in self.nodes(at: at)
+        {
+            if node.name != nil
+            {
+                if node.name=="mmLogo"
+                {
+                    print("Logo click.")
+                    changeGameState(to: GAMESTATE.CHOOSERACE)
+                }
+            } // if the name is not nil
+        } // for each node
         
         
     } // handleClickMM
@@ -76,10 +118,60 @@ class GameScene: SKScene {
         }
     }
     
+    func loadMainMenu()
+    {
+        let tempBG=SKSpriteNode(imageNamed: "mmTempBack01")
+        let tempLogo=SKSpriteNode(imageNamed: "tempLogo")
+        let tempFrame=SKSpriteNode(imageNamed: "mmMenuFrame01")
+        
+        gameState=GAMESTATE.MAINMENU
+        mmAnchor.addChild(tempBG)
+        
+        tempLogo.position = CGPoint(x: -size.width*0.35, y: size.height*0.35)
+        tempLogo.zPosition=1
+        tempLogo.name="mmLogo"
+        mmAnchor.addChild(tempLogo)
+        
+        tempFrame.position.x = -size.width*0.35
+        tempFrame.position.y = -size.height*0.1
+        tempFrame.zPosition=2
+        mmAnchor.addChild(tempFrame)
+    } // load main menu
+    
+    func loadChooseRaceScreen()
+    {
+        var crTemp=SKSpriteNode(imageNamed: "crTemp")
+        crAnchor.addChild(crTemp)
+        gameState=GAMESTATE.CHOOSERACE
+        
+    } // loadChooseRaceScreen
+    
+    func loadStartRaceScreen()
+    {
+        var srTemp=SKSpriteNode(imageNamed: "srTemp")
+        srAnchor.addChild(srTemp)
+        gameState=GAMESTATE.STARTRACE
+    } // loadStartRaceScreen
+    
+    func loadInRaceScreen()
+    {
+        var irTemp=SKSpriteNode(imageNamed: "irTemp")
+        irAnchor.addChild(irTemp)
+        gameState=GAMESTATE.INRACE
+        
+    } // loadInRaceScreen
+    
+    func loadEndRaceScreen()
+    {
+        var erTemp=SKSpriteNode(imageNamed: "erTemp")
+        erAnchor.addChild(erTemp)
+        gameState=GAMESTATE.ENDRACE
+    }
+    
     func changeGameState(to: Int)
     {
         // This function will change the gameState from one state to another.
-        // NOTE: NEVER change gameState directly. ALWAYS call this function to change.
+        // NOTE: NEVER change gameState directly. ALWAYS call this function to change, not the functions that may load certain screens (such as 'loadMainMenu()'). This is important because there will be clean up of screens that we're leaving in this function, as well as (possibly) loading new assets, etc.
         
         if (gameState != to)
         {
@@ -89,29 +181,50 @@ class GameScene: SKScene {
             case GAMESTATE.STARTUP:
                 if (to == GAMESTATE.MAINMENU)
                 {
-                    
-                    let tempBG=SKSpriteNode(imageNamed: "mmTempBack01")
-                    let tempLogo=SKSpriteNode(imageNamed: "tempLogo")
-                    let tempFrame=SKSpriteNode(imageNamed: "mmMenuFrame01")
-                    
-                    gameState=to
-                    mmAnchor.addChild(tempBG)
-                    
-                    tempLogo.position = CGPoint(x: -size.width*0.35, y: size.height*0.35)
-                    tempLogo.zPosition=1
-                    mmAnchor.addChild(tempLogo)
-                    
-                    tempFrame.position.x = -size.width*0.35
-                    tempFrame.position.y = -size.height*0.1
-                    tempFrame.zPosition=2
-                    mmAnchor.addChild(tempFrame)
+                    loadMainMenu()
+ 
                 } // to Main Menu
                 else
                 {
                     print("Error - Unsupported state change: \(gameState) to \(to)")
                 }
-                
             
+            case GAMESTATE.MAINMENU:
+                if(to==GAMESTATE.CHOOSERACE)
+                {
+                    mmAnchor.removeAllChildren()
+                    loadChooseRaceScreen()
+                } // to Choose Race Screen
+                
+            case GAMESTATE.CHOOSERACE:
+                if(to==GAMESTATE.STARTRACE)
+                {
+                    crAnchor.removeAllChildren()
+                    loadStartRaceScreen()
+                }
+            
+            case GAMESTATE.STARTRACE:
+                if(to==GAMESTATE.INRACE)
+                {
+                    srAnchor.removeAllChildren()
+                    loadInRaceScreen()
+                }
+                
+            case GAMESTATE.INRACE:
+                if(to==GAMESTATE.ENDRACE)
+                {
+                    irAnchor.removeAllChildren()
+                    loadEndRaceScreen()
+                }
+                
+            case GAMESTATE.ENDRACE:
+                if(to==GAMESTATE.CHOOSERACE)
+                {
+                    erAnchor.removeAllChildren()
+                    loadChooseRaceScreen()
+                }
+                
+                
             default:
                 print("Error - Unsupported current gameState: \(gameState)")
                 break
--- OffroadRacer/Helper Files/Definitions.swift
+++ OffroadRacer/Helper Files/Definitions.swift
@@ -12,13 +12,13 @@ struct GAMESTATE
     
     static let STARTUP:Int = -1
     static let MAINMENU:Int=0
-    static let INGAME:Int=2
+    static let INRACE:Int=2
     static let PAUSED:Int=4
     static let UPGRADE:Int=6
     static let SCOUTING:Int=8
-    static let RACESTART:Int=10
-    static let RACEEND:Int=12
-    static let RACEBROWSE:Int=14
+    static let STARTRACE:Int=10
+    static let ENDRACE:Int=12
+    static let CHOOSERACE:Int=14
 } // GAMESTATE
 
 
